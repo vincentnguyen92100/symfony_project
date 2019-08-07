437b4d9b1feead61a449cf0c65664293c111f64c
L'ours <walid.siala@coding-academy.fr>
2019-08-07 11:23:46 +0200

    update routes names

--- allocine/src/Controller/SiteController.php
+++ allocine/src/Controller/SiteController.php
@@ -24,10 +24,27 @@ class SiteController extends AbstractController
         return $this->render('site/home.html.twig');
     }
 
+     /**
+     * @route("/site/register", name="site_register")
+     */
+    public function register()
+    {
+        return $this->render('site/register.html.twig');
+    }
+
     /**
-     * @Route("/site/movie/12", name="site_show")
+     * @route("/site/login", name="site_login")
+     */
+    public function login()
+    {
+        return $this->render('site/login.html.twig');
+    }
+    /**
+     * @Route("/site/movie/12", name="site_moviedetails")
      */
     public function show(){
-        return $this->render('site/show.html.twig');
+        return $this->render('site/moviedetails.html.twig');
     }
+
+   
 }
