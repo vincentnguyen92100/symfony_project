950a6b405fd153c2998cff1c48f28d775d34de38
L'ours <walid.siala@coding-academy.fr>
2019-08-07 19:31:55 +0200

    push avant merge, on sert les fesses

--- allocine/src/Controller/SecurityController.php
+++ allocine/src/Controller/SecurityController.php
@@ -49,4 +49,9 @@ class SecurityController extends AbstractController
     public function login(){
         return $this->render('security/login.html.twig');
     }
+
+     /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(){}
 }
