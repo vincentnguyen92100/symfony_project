ddce58e0453058ae13bfa42d639bef0d9d7dd923
L'ours <walid.siala@coding-academy.fr>
2019-08-09 12:30:20 +0200

    fiche films

--- allocine/src/Controller/ApiMovieController.php
+++ allocine/src/Controller/ApiMovieController.php
@@ -24,12 +24,31 @@ class ApiMovieController extends AbstractController
         // die();
         //dump($MoviesTab);
         //$MoviesObjets=$serializer->denormalize($MoviesTab, 'App\Entity\Region[]')
-        
+        //die();
         return $this->render('api_movie/index.html.twig', [
             'Movies'=> $MoviesTab,'genres'=> $categoryTab
         ]);
         // dump($categoryTab);
         // die();
+    }
+
+    /**
+     * @Route("/movies/{id}", name="movies_details")
+     */
+    public function showMovies(SerializerInterface $serializer, $id)
+    {
+        $moviesDetails=file_get_contents('https://api.themoviedb.org/3/movie/'.$id.'?language=en-US&api_key=f5621d217c7c61f28b699c88eade6ebf');
+        $MoviesTab=$serializer->decode($moviesDetails, 'json');
+
+        $trailerDetails=file_get_contents('https://api.themoviedb.org/3/movie/'.$id.'/videos?language=en-US&api_key=f5621d217c7c61f28b699c88eade6ebf');
+        $trailer=$serializer->decode($trailerDetails, 'json');
+        //dump($threaler);
+        //die();
+        //$details = $moviesDetails->find($id);
+        return $this->render('api_movie/details.html.twig', [
+        'details'=> $MoviesTab, 'trailer'=> $trailer
+        ]);
+        
     }
 
 
